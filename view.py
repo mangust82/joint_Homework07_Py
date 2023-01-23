53e04c99128f8abfda623b0ce54f569a92ebda58
da <da@mail.ru>
2023-01-23 19:36:20 +0300

    __modified view.py

--- view.py
+++ view.py
@@ -1,16 +1,22 @@
 def main_menu():
-    print('1. ')
-    print('2. ')
-    print('3. ')
-    print('4. ')
-    print('5. ')
-    print('6. ')
-    print('7. ')
+    print('1. Показать справочник контактов (жми 1) ')
+    print('2. Поиск (жми 2) ')
+    print('3. Импорт базы контактов (жми 3) ')
+    print('4. Экспорт базы контактов (жми 4) ')
+    print('5. Добавить новый контакт (жми 5)  ')
+    print('6. Удалить контакт (жми 6)  ')
+    print('7. Редактирование контакта (жми 7) ')
+    print('8. Для выхода жми 8 \n')
     menu = input('Выберите пункт меню: ')
     return menu
 
 def show_database(list_of_dict):
-    pass
+    #pass
+    for idx, el in enumerate(list_of_dict):
+
+        man = list(el.values())
+        print(f'{idx:4}  Фамилия: {man[0]:10} Имя: {man[1]:8} Номер: {man[2]:10} Комментарий: {man[3]: 3}')
+
 
 def second_menu(text):
     a = input(text)
@@ -23,3 +29,6 @@ def add_user():
     coment = input('Введите комментарий: ')
     user_dict = {}
     return user_dict
+
+# вызов меню(тест)
+main_menu()
