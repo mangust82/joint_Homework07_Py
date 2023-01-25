1eab07206ba4dc1dfef019baf46c1a21303efb43
Alfabit7 <tritg@yandex.ru>
2023-01-25 11:04:12 +0300

    clone new repo

--- controller.py
+++ controller.py
@@ -1,16 +1,26 @@
-import init
 import view
-import find
-import mod_imp
-import mod_exp
-import edit_db
+import model
+import csv  # ( добавила импорт csv тк без него него не вызывается csv.writer
+# и вообще это моя unload_fileфункция)
+
+
+def SaveDataBase(name_data_base, data_base):
+    field_names = ['Фамилия', 'Имя', 'Телефон', 'Комментарий']
+    addList = []
+    for dataUser in data_base:
+        addList.append(dataUser)
+    with open(name_data_base, 'w', newline='', encoding='UTF-8') as csvfile:
+        writer = csv.writer(
+            csvfile, fieldnames=field_names, quoting=csv.QUOTE_ALL, delimiter=';')
+        for i in addList:
+            writer.writerows(i)
 
 
 path = 'database.csv'
 
 
 def ClickButton():
-    data_base = init.init(path)
+    data_base = model.load_file(path)[1]
     while True:
 
         num_menu = view.main_menu()
@@ -21,10 +31,11 @@ def ClickButton():
             view.show_database(data_base)
         elif num_menu == '2':
             find_str = view.second_menu('Введите строку поиска: ')
-            find.find_cont(find_str, data_base)
+            model.find_cont(find_str, data_base)
         elif num_menu == '3':
             path_file = view.second_menu('Выберите файл для импорта: ')
-            if mod_imp.db_update(path_file):
+            status, data_base = model.ChangeBase(path_file, data_base)
+            if status:
                 print('Файл успешно загружен')
             else:
                 print(
@@ -33,23 +44,29 @@ def ClickButton():
         elif num_menu == '4':
             file_name = view.second_menu(
                 'Выберите имя экспортируемого файла: ')
-            mod_exp.unload_file(file_name, data_base)
+            model.unload_file(file_name, data_base)
 
         elif num_menu == '5':
-            data_base = edit_db.add_cont(data_base, view.add_user())
-            mod_exp.unload_file('database.csv', data_base)
+            data_base = model.add_cont(data_base, view.add_user())
+            model.unload_file('database.csv', data_base)
 
         elif num_menu == '6':
-            num_user = view.second_menu(
-                'Введите строку удаляемого контакта: ')
-            data_base = edit_db.del_cont(data_base, num_user)
-            mod_exp.unload_file('database.csv', data_base)
+            num_user = int(view.second_menu(
+                'Введите строку удаляемого контакта: '))
+            data_base = model.del_cont(data_base, num_user)
+            model.unload_file('database.csv', data_base)
 
         elif num_menu == '7':
-            num_user = view.second_menu(
-                'Введите строку редактируемого контакта: ')
-            data_base = edit_db.edit_cont(data_base, num_user)
-            mod_exp.unload_file('database.csv', data_base)
+            edit_user_db = []
+            num_user = int(view.second_menu(
+                'Введите строку редактируемого контакта: '))
+            edit_user_db.append(data_base[num_user-1])
+            view.show_str(edit_user_db)
+            data_base = model.edit_cont(data_base, view.add_user(), num_user-1)
+            model.unload_file('database.csv', data_base)
+            # data_base = edit_db.edit_cont(data_base, num_user)
+            # mod_exp.unload_file('database.csv', data_base)
 
         elif num_menu == '8':
-            data_base = edit_db.edit_cont(data_base, num_user)
+            model.unload_file('databaseSave.csv', data_base)
+# поставила свою функию сюда вместо SaveDataBase и вроде работает,
